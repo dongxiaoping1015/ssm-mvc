c637fb2c194c2fda878b12124a9c3d65bc0a82f8
dongxiaoping1015 <xiaoping_1015@163.com>
2018-11-04 14:17:58 +0800

    将Spring改为注解

--- src/dong/service/StudentServiceImpl.java
+++ src/dong/service/StudentServiceImpl.java
@@ -2,14 +2,21 @@ package dong.service;
 
 import dong.beans.Student;
 import dong.dao.IStudentDao;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.annotation.Resource;
+
+@Service("studentService")
 public class StudentServiceImpl implements IStudentService {
+    @Resource(name = "IStudentDao")
     private IStudentDao dao;
 
     public void setDao(IStudentDao dao) {
         this.dao = dao;
     }
 
+    @Transactional
     @Override
     public void addStudent(Student student) {
         dao.insertStudent(student);
